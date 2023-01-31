16c6724383bd9bdbf33067d50d49c742d2286328
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-01-31 20:34:47 +0100

    Send a message with the gui

--- src/app/ConsumerContainer.java
+++ src/app/ConsumerContainer.java
@@ -1,7 +1,9 @@
 package app;
 
+import app.agents.ConsumerAgent;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
+import jade.gui.GuiEvent;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
@@ -22,20 +24,32 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class ConsumerContainer extends Application{
+	
+	private ConsumerAgent consumerAgent;
+	
+	public ConsumerAgent getConsumerAgent() {
+		return consumerAgent;
+	}
+
+	public void setConsumerAgent(ConsumerAgent consumerAgent) {
+		this.consumerAgent = consumerAgent;
+	}
 
 	public static void main(String[] args) {
+		launch(ConsumerContainer.class);
+	}
+	
+	public void StartContainer() {
 		try {
-			String book;
-			book = "XML";
 			Runtime runtime = Runtime.instance();
 			ProfileImpl profileImpl = new ProfileImpl(false);
 			profileImpl.setParameter(ProfileImpl.MAIN_HOST, "localhost");
 			AgentContainer container = runtime.createAgentContainer(profileImpl);
 			/* Create and agent and add it to the container */
 			AgentController agentController = container.createNewAgent
-					("Consumer-1", "app.agents.ConsumerAgent", new Object[] {book});
+					("Consumer-1", "app.agents.ConsumerAgent", new Object[] {this});
 			agentController.start();
-			launch(ConsumerContainer.class);
+			
 		} catch (ControllerException e) {
 			e.printStackTrace();
 		}
@@ -44,6 +58,7 @@ public class ConsumerContainer extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		StartContainer();
 		primaryStage.setTitle("Consumateur");
 		BorderPane borderPane = new BorderPane();
 		HBox hBox = new HBox(); 
@@ -76,7 +91,9 @@ public class ConsumerContainer extends Application{
 			@Override
 			public void handle(ActionEvent event) {
 				String livre = textFieldLivre.getText();
-				observbleList.add(livre);
+				GuiEvent guiEvent = new GuiEvent(this, 1);
+				guiEvent.addParameter(livre);
+				consumerAgent.onGuiEvent(guiEvent);
 			}
 		});
 	}
--- src/app/agents/ConsumerAgent.java
+++ src/app/agents/ConsumerAgent.java
@@ -1,11 +1,20 @@
 package app.agents;
 
+import app.ConsumerContainer;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+import jade.lang.acl.ACLMessage;
 import jade.wrapper.ControllerException;
 
-public class ConsumerAgent extends Agent{
+public class ConsumerAgent extends GuiAgent{
+	private ConsumerContainer gui;
+	
 	@Override
 	protected void setup() {
+		gui = (ConsumerContainer) getArguments()[0];
+		gui.setConsumerAgent(this);
 		System.out.println("Run the agent : " + this.getAID().getName());
 	}
 	
@@ -32,5 +41,17 @@ public class ConsumerAgent extends Agent{
 	@Override
 	protected void takeDown() {
 		System.out.println("Before Die...");
+	}
+
+	@Override
+	public void onGuiEvent(GuiEvent guiEvent) {
+		
+		if (guiEvent.getType() == 1 ) {
+			ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
+			String livre = guiEvent.getParameter(0).toString();
+			aclMessage.setContent(livre);
+			aclMessage.addReceiver(new AID("rma", AID.ISLOCALNAME));
+			send(aclMessage);
+		}
 	}
 }
