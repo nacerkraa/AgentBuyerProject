6edf847b22e7eb7fb392d4df163570d113ddef27
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-02-03 17:38:57 +0100

    Update BuyerAgent.java

--- src/app/agents/BuyerAgent.java
+++ src/app/agents/BuyerAgent.java
@@ -5,6 +5,12 @@ import app.ConsumerContainer;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.ParallelBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.introspection.AddedBehaviour;
 import jade.gui.GuiAgent;
 import jade.gui.GuiEvent;
@@ -14,27 +20,56 @@ import jade.wrapper.ControllerException;
 
 public class BuyerAgent extends GuiAgent{
 	private BuyerContainer gui;
-	
+	private AID[] listSellers;
 	@Override
 	protected void setup() {
 		
 		gui = (BuyerContainer) getArguments()[0];
 		gui.setBuyerAgent(this);
 		System.out.println("Run the agent : " + this.getAID().getName());
-		addBehaviour(new CyclicBehaviour() {
+		ParallelBehaviour parallelBehaviour = new ParallelBehaviour();
+		addBehaviour(parallelBehaviour);
+		parallelBehaviour.addSubBehaviour(new TickerBehaviour(this, 6000) {
+			
+			@Override
+			protected void onTick() {
+				try {
+					DFAgentDescription description = new DFAgentDescription();
+					ServiceDescription serviceDescription = new ServiceDescription();
+					serviceDescription.setType("Sell");
+					description.addServices(serviceDescription);
+					DFAgentDescription[] agentDescriptions = DFService.search(myAgent, description);
+					listSellers = new AID[agentDescriptions.length];
+					for(int i=0; i< agentDescriptions.length; i++) {
+						listSellers[i] = agentDescriptions[i].getName();
+					}
+				} catch (FIPAException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		parallelBehaviour.addSubBehaviour(new CyclicBehaviour() {
 			
 			@Override
 			public void action() {
 				MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 				ACLMessage message = receive(messageTemplate);
 				if (message != null) {
-					System.out.println("Reciption d'un message "+ message.getContent());
+					System.out.println("Recieve a message "+ message.getContent());
 					GuiEvent guiEvent = new GuiEvent(this, 1);
-					guiEvent.addParameter(message.getContent());
+					String bookName = message.getContent();
+					guiEvent.addParameter(bookName);
 					gui.viewMessage(guiEvent);
 					/*
 					 * Book Buyer Operation
 					 */
+					ACLMessage aclMessage = new ACLMessage(ACLMessage.CFP);
+					aclMessage.setContent(bookName);
+					for(AID aid:listSellers) {
+						aclMessage.addReceiver(aid);
+					}
+					send(aclMessage);
 				}
 				
 			}
